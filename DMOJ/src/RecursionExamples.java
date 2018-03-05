08f320aed207219c693e03110faf21a1978c81ed
323843193 <323843193@C-5Q3ZJS1.tdsb.on.ca>
2018-03-05 15:00:04 -0500

    Push

--- DMOJ/src/RecursionExamples.java
+++ DMOJ/src/RecursionExamples.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
+/**
+ * RecursionExamples.java
+ * Solving different programs using recursive methods.
+ * March 5th, 2018
+ * @author WayneChau
+ */
 
 public class RecursionExamples {
 
+	/**
+	 * Tests if the method works by printing the answers derived from the recursive methods.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -13,10 +23,15 @@ public class RecursionExamples {
 		//System.out.println(sum(n));
 		//System.out.println(exponent(a,b));
 		//System.out.println(binToDec(n));
-		System.out.println(decToBin(n));
+		//System.out.println(decToBin(n));
 	}
-
-	private static int factorial (int n) {
+	
+	/**
+	 * Calculates the factorial of the integer, inputed by the user.
+	 * @param n - The number inputed by the user.
+	 * @return The factorial of n
+	 */
+	public static int factorial (int n) {
 		if (n == 1) {
 			return 1;
 		}
@@ -24,8 +39,13 @@ public class RecursionExamples {
 			return n * factorial(n-1);
 		}
 	}
-
-	private static int sum(int n) {
+	
+	/**
+	 * Calculates the sum of the integer between 0 and n, inputed by the user .
+	 * @param n - The number inputed by the user.
+	 * @return returns the sum of all numbers between 0 and n
+	 */
+	public static int sum(int n) {
 		if (n <=1 ) {
 			return 1;
 		}
@@ -33,8 +53,14 @@ public class RecursionExamples {
 			return n + sum (n-1);
 		}
 	}
-
-	private static long exponent (int a, int b) {
+	
+	/**
+	 * Calculate the result of a number raised to the power of another number entered by the user.
+	 * @param a - The base of the exponent.
+	 * @param b - The exponent a is raised to the power of.
+	 * @return returns the result of a raised to the power of b.
+	 */
+	public static long exponent (int a, int b) {
 		if (b==1) {
 			return a;
 		}
@@ -46,52 +72,55 @@ public class RecursionExamples {
 		}
 	}
 
-	private static int binToDec (long n) {
-		int counter = 0;
-		int output = 0;
+	/**
+	 * Passes the decimal value equivalent to the binary.
+	 * @param n - The binary inputed by the user.
+	 * @return returns the decimal equivalent to the binary called from binToDecHelp.
+	 */
+	public static int binToDec (long n) {
+		int counter = 0; //exponent to raise to the power of the base 2
+		int output = 0; //the decimal value equivalent to binary
 
-		return helperMethod(n,counter,output);
+		return binToDecHelp(n,counter,output);
 
 	}
-	private static int helperMethod (long n, int counter, int output){
+	
+	/**
+	 * Calculates the decimal value equivalent to binary.
+	 * @param n - The number inputed by the user.
+	 * @param counter - The exponent the base two is raised to the power of.
+	 * @param output - The decimal answer of the binary n.
+	 * @return returns the decimal equivalent to the binary
+	 */
+	private static int binToDecHelp (long n, int counter, int output){
 		output = 0;
 		if (n%10 == 1) {
 			output = (int) Math.pow(2, counter);
 
 		}
 		if (n==0) {
-			return (int) n;
+			return (int) n; //base case
 		}
 		n=n/10;
-		return output + helperMethod(n,counter+1,output);
+		return output + binToDecHelp(n,counter+1,output);
 	}
-
-	private static long decToBin(int n) {
+	
+	/**
+	 * Calculates the binary equivalent to the decimal value.
+	 * @param n - The number inputed by the user.
+	 * @return returns the binary equivalent to the given decimal number n
+	 */
+	public static long decToBin(int n) {
 		String out = "";
-		return helperMethod2(n, out);
+		if (n>0) {
+			 
+			out += n%2 + ""; //returns 1 or 0 depending on the remainders
+			n = n/2;
+			
+		}
+		else if (n==0){
+			return n; //base case
+		}             
+		return Long.parseLong(decToBin(n)+out);
 	}
-
-	private static long helperMethod2 (int n, String out) {
-
-		
-			if (n%2 == 1 && n >= 1) {
-				out += 1;
-				n = n/2;
-
-			}
-			else if (n%2 == 0 && n >= 1){
-				out += 0;
-				n = n/2;
-			}
-			if (n == 0) {
-				return 0;
-			}
-		
-		return Long.parseLong(out) + helperMethod2(n,out);
-
-
-	}
-
-
-
 }
